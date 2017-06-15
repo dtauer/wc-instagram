2cde1ae4633cb6279a762a0c447dc6c0987a3f22
Dustin Tauer <dustin@dtauer.com>
2017-06-15 11:25:04 -0500

    Added Dynamic Images
    Images are now loaded from unsplash.it

--- scripts.js
+++ scripts.js
@@ -12,18 +12,30 @@ window.addEventListener('scroll', function(){
     }
 })
 
-// Add click events to all the images
-const images = document.querySelectorAll('.image')
-images.forEach(function(image, index){
-    //Add a click event to the image
-    //have the click event console.log(index)
-    image.addEventListener('click', function(e){
-        e.preventDefault() // Stop the click event from refreshing
-        const source = this.querySelector('img').src
-        const id = source.split('=')[1]
-        showFullImage(id)
+function addClickEvents() {
+    // Add click events to all the images
+    const images = document.querySelectorAll('.image')
+    images.forEach(function(image, index){
+        //Add a click event to the image
+        //have the click event console.log(index)
+        image.addEventListener('click', function(e){
+            e.preventDefault() // Stop the click event from refreshing
+            const source = this.querySelector('img').src
+            const id = source.split('=')[1]
+            showFullImage(id)
+        })
     })
-})
+}
+
+// Add Error Events to the images incase the image is not available
+function addErrorEvents(){
+    const images = document.querySelectorAll('.image img')
+    images.forEach( (image, index) => {
+        image.addEventListener('error', () => {
+            image.src = `http://unsplash.it/300/?image=580`
+        })
+    })
+}
 
 // function showFullImage (id) {
 const showFullImage = id => {
@@ -44,3 +56,43 @@ fullContainer.addEventListener('click', function(){
     this.classList.add('hidden')
 })
 
+// Load Data from http://unsplash.it/list
+// then convert to JSON
+// Then grab 20 random images
+// then add images to HTML and call addClickEvents()
+fetch('http://unsplash.it/list')
+.then( result => result.json() )
+.then(result => {
+    let randoms = []
+    for(let i=0; i<20; i++) {
+        randoms.push( Math.round(Math.random()*result.length) )
+    }
+    let images = result.filter(image => {
+        return randoms.includes(image.id)
+    })
+    populateImages(images)
+    addClickEvents()
+    addErrorEvents()
+})
+
+// This function will add all the images we loaded remotely
+// to the HTML page
+function populateImages(imageArray) {
+    // Need a variable for the image container
+    const imageContainer = document.querySelector('.images .inner')
+    //Then we need to loop through the imageArray
+    // maybe just console.log somethign to make sure it working
+    imageArray.forEach( (image, index) => {
+        const html = `<a href="" class="image">
+                        <img src="http://unsplash.it/320/?image=${image.id}" alt="${image.author}">
+                        <span class="image__cover">View Image</span>
+                     </a>`
+        imageContainer.innerHTML += html
+    })
+}
+
+/*
+<a  href="" class="image"><img src="http://unsplash.it/320/?image=99" alt="">
+    <span class="image__cover">View Image</span>
+</a>
+*/
